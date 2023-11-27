f2fea31d333e46172d5944489f3bb8afdedc6fd9
Héctor Paredes Benavides <hectorpbenavides@gmail.com>
2023-11-27 20:30:11 +0100

    v0.0 - Inicialización del main del frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+/*
+    Título: App
+    Descripción: Programa principal para gestionar la aplicación web
+    Fecha: 27/11/2023
+    Última Modificación: 27/11/2023
+*/
+
+/* Importado de Bibliotecas */
+// Bibliotecas externas
+import React from "react";
+import {
+    Routes,
+    Route,
+    BrowserRouter
+} from "react-router-dom";
+
+// Bibliotecas de componentes propios
+import Home from './components/home/Home'
+
+/* Componente Principal */
+function App(){
+
+    // Retornamos las rutas
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+
 }
 
 export default App;
---
+++ frontend/src/components/home/Home.jsx
@@ -0,0 +1,29 @@
+
+
+/*
+    Título: Home
+    Descripción: Creamos un componente que albergue la landing page de nuestra aplicación
+    Fecha: 27/11/2023
+    Última Modificación: 27/11/2023
+*/
+
+/* Importado de Bibliotecas */
+// Bibliotecas externas
+import React from "react";
+
+// Bibliotecas de componentes propios
+
+
+/* Componente Principal */
+function Home(){
+
+    // Retornamos el código HTML
+    return(
+        <>
+            <h1>Hola mundo desde AñadaMaster!</h1>
+        </>
+    );
+
+}
+
+export default Home;
